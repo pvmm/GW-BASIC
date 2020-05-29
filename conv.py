6a34af1d12e73deea99ee878c3ba020b2dcc3118
Leandro Pereira <leandro@hardinfo.org>
2020-05-28 17:59:59 -0700

    Improve pattern matcher for Transformer step

--- conv.py
+++ conv.py
@@ -3,6 +3,7 @@
 # Licensed under GPLv2.
 
 from collections import deque
+from itertools import tee, chain, repeat
 import sys
 import traceback
 
@@ -1182,47 +1183,188 @@ class Parser:
 
             return self._error("Don't know how to parse token %s" % token)
 
+
+def windowed(seq, n, fillvalue=None, step=1):
+    """Return a sliding window of width *n* over the given iterable.
+        >>> all_windows = windowed([1, 2, 3, 4, 5], 3)
+        >>> list(all_windows)
+        [(1, 2, 3), (2, 3, 4), (3, 4, 5)]
+    When the window is larger than the iterable, *fillvalue* is used in place
+    of missing values:
+        >>> list(windowed([1, 2, 3], 4))
+        [(1, 2, 3, None)]
+    Each window will advance in increments of *step*:
+        >>> list(windowed([1, 2, 3, 4, 5, 6], 3, fillvalue='!', step=2))
+        [(1, 2, 3), (3, 4, 5), (5, 6, '!')]
+    To slide into the iterable's items, use :func:`chain` to add filler items
+    to the left:
+        >>> iterable = [1, 2, 3, 4]
+        >>> n = 3
+        >>> padding = [None] * (n - 1)
+        >>> list(windowed(chain(padding, iterable), 3))
+        [(None, None, 1), (None, 1, 2), (1, 2, 3), (2, 3, 4)]
+
+    Shamelessly stolen from more-itertools.
+    """
+    if n < 0:
+        raise ValueError('n must be >= 0')
+    if n == 0:
+        yield tuple()
+        return
+    if step < 1:
+        raise ValueError('step must be >= 1')
+
+    window = deque(maxlen=n)
+    i = n
+    for _ in map(window.append, seq):
+        i -= 1
+        if not i:
+            i = step
+            yield tuple(window)
+
+    size = len(window)
+    if size < n:
+        yield tuple(chain(window, repeat(fillvalue, n - size)))
+    elif 0 < i < min(step, n):
+        window += (fillvalue,) * i
+        yield tuple(window)
+
 class Transformer:
     def __init__(self, parser):
         self.parser = parser
 
-    def transform(self):
-        ignore_count = 0
-
-        for token in self.parser.parse():
-            if ignore_count:
-                if 'comment' in token and token['comment']:
-                    token = {'type': 'comment', 'value': token['comment']}
-                else:
-                    ignore_count -= 1
+    def _match(self, window, *match):
+        def _match_op_tuple(code_oper, pattern_oper):
+            for pattern, code in zip(pattern_oper, code_oper):
+                if pattern is None:
                     continue
-            elif token['type'] == 'instruction':
-                if self._is_jump_skip_ret(token):
-                    ignore_count = 1 # Ignore RET
-                    token = {'type': 'instruction', 'op': 'ret_' + token['op'], 'operands': [], 'comment': token['comment']}
-                elif self._is_xthl(token):
-                    ignore_count = 2 # Ignore XCHG SI, BX / PUSH SI
-                    token = {'type': 'instruction', 'op': 'xthl', 'operands': [], 'comment': token['comment']}
-                elif self._is_rep(token):
-                    to_repeat = token['operands'][0].lower()
-                    token = {'type': 'instruction', 'op': token['op'].lower() + '_' + to_repeat, 'operands': [], 'comment': token['comment']}
+                if isinstance(pattern, set):
+                    if not code in pattern:
+                        return False
+                elif isinstance(pattern, str):
+                    if code_oper != pattern_oper:
+                        return False
+                elif isinstance(pattern, tuple):
+                    if type(code) != type(pattern):
+                        return False
+                    if not _match_op_tuple(code, pattern):
+                        return False
+                else:
+                    raise NotImplementedError("Pattern of type %s not supported" % type(pattern))
+            return True
 
+        def _match_op(code, pattern):
+            code_op, code_oper = code['op'], code['operands']
+            pattern_op, pattern_oper = pattern
+
+            if isinstance(pattern_op, str):
+                op_match = code_op == pattern_op
+            elif isinstance(pattern_op, set):
+                op_match = code_op in pattern_op
+            elif pattern_op is None:
+                op_match = True
+            else:
+                raise NotImplementedError("Operation of type %s not supported" % type(pattern_op))
+
+            if isinstance(pattern_oper, tuple):
+                oper_match = _match_op_tuple(code_oper, pattern_oper)
+            elif pattern_oper is None:
+                oper_match = True
+            else:
+                raise NotImplementedError("Operand of type %s not supported" % type(pattern_oper))
+
+            return op_match and oper_match
+
+        return len(match) if all(_match_op(wop, mop) for wop, mop in zip(window[:len(match)], match)) else -1
+
+    def _calculate_transform_dict(self, tokens):
+        trans_dict = {}
+
+        def fill_dict(n, start_id, *with_items):
+            if len(with_items) > n:
+                raise NotImplementedError("Can't insert more instructions than the window size")
+            for i in range(start_id, start_id+n):
+                trans_dict[i] = None
+            for i, item in enumerate(with_items, start=start_id):
+                trans_dict[i] = item
+
+        for window in windowed((token for token in tokens if token['type'] == 'instruction'), 4):
+            if window[0] is None:
+                break
+
+            start_id = window[0]['id']
+
+            matched = self._match(window, ('POP', ('AX',)), ('XCHG', ('AL', 'AH')), ('SAHF', ()))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'restorepsw', 'operands': ('stack',)})
+                continue
+
+            matched = self._match(window, ('POP', ('SI',)), ('XCHG', ('SI', 'BX')), ('PUSH', ('SI',)))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'xthl', 'operands': ()})
+                continue
+
+            matched = self._match(window, ('XOR', ('AH', 'AH')), ('CMP', ('AL', None)), ('JZ', None))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'cmp', 'operands': window[1]['operands']}, {'op': 'jz', 'operands': window[2]['operands']})
+                continue
+
+            matched = self._match(window, ('POP', ('AX',)), ('OR', ('AH', 'AH')), ('JZ', None))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'jz', 'operands': window[2]['operands']})
+                continue
+
+            matched = self._match(window, ('LAHF', ()), ({'INC', 'DEC'}, ({'BX', 'CX', 'DX'},)), ('SAHF', ()))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': window[1]['op'], 'operands': window[1]['operands']})
+                continue
+
+            matched = self._match(window, ('LAHF', ()), ('XCHG', ('AL', 'AH')), ('PUSH', ('AX',)), ('XCHG', ('AL', 'AH')))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'savepsw', 'operands': ('stack',)})
+                continue
+
+            matched = self._match(window, ({'JZ', 'JNZ', 'JNAE', 'JNB', 'JS'}, (('SHORT', '$+3'),)))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'ret_' + window[0]['op'], 'operands': ()})
+                continue
+
+            matched = self._match(window, ({'REP', 'REPE', 'REPZ', 'REPNZ', 'REPNE'}, ({'LODSB', 'LODSW', 'STOSB', 'STOSW', 'MOVSB', 'MOVSW', 'SCASB', 'SCASW', 'CMPSB'},)))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': window[0]['op'] + '_' + window[0]['operands'][0], 'operands': ()})
+                continue
+
+            matched = self._match(window, ({'DEC', 'DECB'}, ('CH',)), ('JNZ', ('SHORT', None)))
+            if matched >= 0:
+                fill_dict(matched, start_id, {'op': 'djnz', 'operands': window[1]['operands']})
+                continue
+
+        return trans_dict
+
+    def transform(self):
+        def annotate_with_id():
+            id = 0
+            for token in self.parser.parse():
+                token['id'] = id
+                yield token
+                id += 1
+
+        left, right = tee(annotate_with_id())
+        trans_dict = self._calculate_transform_dict(left)
+        for token in right:
+            if token['id'] not in trans_dict:
+                yield token
+                continue
+
+            trans = trans_dict[token['id']]
+            if trans is None:
+                comment = token.get('comment', None)
+                if comment is not None:
+                    yield {'type': 'comment', 'value': comment}
+                continue
+
+            token.update(trans)
             yield token
-
-    def _is_rep(self, token):
-        if token['op'] not in {'REP', 'REPE', 'REPZ', 'REPNZ', 'REPNE'}:
-            return False
-        if len(token['operands']) == 1:
-            return token['operands'][0].upper() in {'LODSB', 'LODSW', 'STOSB', 'STOSW', 'MOVSB', 'MOVSW', 'SCASB', 'SCASW'}
-        return False
-
-    def _is_jump_skip_ret(self, token):
-        if token['op'] not in {'JZ', 'JNZ', 'JNAE', 'JNB', 'JS'}:
-            return False
-        return token['operands'] == (('SHORT', '$+3'),)
-
-    def _is_xthl(self, token):
-        return token['op'] == 'POP' and token['operands'] == ('SI',)
 
 class PasmoWriter:
     regmap = {
@@ -1941,6 +2083,16 @@ class PasmoWriter:
     def _gen_instruction_repe_scasb(self, token):
         # FIXME: what to do here?
         return '; REPE SCASB'
+
+    def _gen_instruction_savepsw(self, token):
+        if token['operands'] == ('stack',):
+            return 'PUSH AF'
+        raise SyntaxError("Internal error: operands for savepsw are invalid: %s" % str(token))
+
+    def _gen_instruction_restorepsw(self, token):
+        if token['operands'] == ('stack',):
+            return 'POP AF'
+        raise SyntaxError("Internal error: operands for savepsw are invalid: %s" % str(token))
 
     def _gen_instruction(self, token):
         op = token['op']
